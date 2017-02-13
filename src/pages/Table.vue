e2e50c5b7d3a950359ec18f70dffdfe2d36d0341
wangshuaijie <hzwsj@tairanchina.com>
2017-02-13 19:11:42 +0800

    .

---
+++ src/pages/Table.vue
@@ -0,0 +1,44 @@
+  <template>
+    <div>
+      <SideBar></SideBar>
+      <el-table :data="tableData" style="width: 100%;margin-top:30px;">
+        <el-table-column  prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name"label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+    </div>
+  </template>
+
+
+ <script>
+  import SideBar from './SideBar'
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
+    components:{
+      SideBar
+    }
+  }
+</script>
+<style type="text/css">
+  /*@import url("//unpkg.com/element-ui/lib/theme-default/index.css");*/
+</style>
---
+++ src/pages/login.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<div v-if='DONE_LOGIN_INFO == "登录"'>
+			用户名:<input type="text"  v-model="loginData.phone"><br/>
+			密码:<input type="text" v-model="loginData.password">
+			<div id="err-msg"></div>
+			<button @click="login()">登录</button>
+		</div>
+		<div v-if='DONE_LOGIN_INFO != "登录"'>
+				<p>用户名：{{DONE_LOGIN_INFO.phone}}</p>
+				<p>用户ID：<span style="font-size:0.75rem">{{DONE_LOGIN_INFO.userId}}</span></p>
+			<button @click="logOut()">点击退出</button>
+		</div>
+	</div>
+</template>
+
+
+<script type="text/javascript">
+	import {
+		mapGetters
+	} from 'vuex'
+	export default {
+		data: function() {
+			return {
+				loginData:{}
+			}
+		},
+		name: 'Login',
+		components: {
+			
+		},
+		computed: {
+			...mapGetters(['DONE_LOGIN_INFO'])
+		},
+		methods:{
+			login: function(){
+				this.$store.dispatch('POST_LOGIN_INFO',this.loginData)
+
+				console.log(this.$route);
+			},
+			logOut :function(){
+				// router.replace('/app')
+				window.location.href='/app'
+				//this.$router.push('/app');
+			}
+		}
+	}
+</script>
+
+
+<style type="text/css">
+	
+</style>
